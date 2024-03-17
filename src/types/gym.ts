c293aac5c7c71d79754febcc2d766b38f144f254
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-03-17 19:49:35 +0100

    ✨ Add types

---
+++ src/types/gym.ts
@@ -0,0 +1,35 @@
+import { Offer } from './offer';
+
+export interface Gym {
+    name: string;
+    id: string;
+    logo: string;
+    agbs?: string;
+    offers: Offer[];
+    abonnements: Offer[];
+    contact?: Contact;
+    location?: Location;
+    openingTimes?: OpeningTime[];
+}
+
+export interface Contact {
+    phone: string;
+    email: string;
+    socialMedia: ISocialMedia;
+}
+
+export interface ISocialMedia {
+    [key: string]: string;
+}
+
+export interface Location {
+    address: string;
+    coordinates: [number, number];
+}
+
+export interface OpeningTime {
+    [key: string]: {
+        start: Date;
+        end: Date;
+    };
+}
