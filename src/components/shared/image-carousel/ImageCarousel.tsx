6b270787fab7cbbfc363be3a6f1999fbf8379855
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-03-30 19:55:43 +0100

    ✨ Add image carousel

--- src/components/shared/image-carousel/ImageCarousel.tsx
+++ src/components/shared/image-carousel/ImageCarousel.tsx
@@ -1,4 +1,6 @@
-import { ReactElement, useMemo } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { AnimatePresence, motion, wrap } from 'framer-motion';
+import './imageCarousel.scss';
 
 interface ImageCarouselProps {
     images: {
@@ -7,18 +9,68 @@ interface ImageCarouselProps {
     }[];
 }
 
+const variants = {
+    enter: (direction: number) => {
+        return {
+            x: direction > 0 ? 1000 : -1000,
+            opacity: 0
+        };
+    },
+    center: {
+        zIndex: 1,
+        x: 0,
+        opacity: 1
+    },
+    exit: (direction: number) => {
+        return {
+            zIndex: 0,
+            x: direction < 0 ? 1000 : -1000,
+            opacity: 0
+        };
+    }
+};
+
 const ImageCarousel = ({ images }: ImageCarouselProps) => {
-    const content = useMemo(() => {
-        const items: ReactElement[] = [];
+    const [[page, direction], setPage] = useState([0, 0]);
 
-        images.forEach(({ id, url }) => {
-            items.push(<div>test</div>);
-        });
+    const imageIndex = wrap(0, images.length, page);
 
-        return items;
-    }, [images]);
+    const paginate = useCallback(
+        (newDirection: number) => {
+            setPage([page + newDirection, newDirection]);
+        },
+        [page]
+    );
 
-    return <div>test</div>;
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            paginate(1);
+        }, 5000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [paginate]);
+
+    return (
+        <div className="image-carousel">
+            <AnimatePresence initial={false} custom={direction}>
+                <motion.img
+                    key={page}
+                    src={images[imageIndex].url}
+                    custom={direction}
+                    variants={variants}
+                    initial="enter"
+                    animate="center"
+                    exit="exit"
+                    transition={{
+                        x: { type: 'spring', stiffness: 300, damping: 30 },
+                        opacity: { duration: 0.2 }
+                    }}
+                />
+            </AnimatePresence>
+        </div>
+    );
 };
 
 ImageCarousel.displayName = 'ImageCarousel';
