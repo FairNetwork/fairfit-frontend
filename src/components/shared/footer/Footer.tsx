4147d5096fceda70aff6a87e799b02143164942c
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-04-03 20:15:31 +0200

    ✨ Add footer

--- src/components/shared/footer/Footer.tsx
+++ src/components/shared/footer/Footer.tsx
@@ -1,9 +1,37 @@
 import './footer.scss';
+import { ReactElement, useMemo } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FooterItem } from '../../../types/footer';
 
-const Footer = () => {
-    return <div className="footer">Footer</div>;
+interface FooterProps {
+    items: FooterItem[];
+}
+
+const Footer = ({ items }: FooterProps) => {
+    const navigate = useNavigate();
+
+    const content = useMemo(() => {
+        const renderedItems: ReactElement[] = [];
+
+        items.forEach(({ path, id, name }) => {
+            renderedItems.push(
+                <div
+                    className="footer__item"
+                    key={`footer-item__${id}`}
+                    onClick={() => {
+                        navigate(`/${path}`);
+                    }}>
+                    {name}
+                </div>
+            );
+        });
+
+        return renderedItems;
+    }, [items, navigate]);
+
+    return <div className="footer">{content}</div>;
 };
 
 Footer.displayName = 'Footer';
 
-// export default Footer;
+export default Footer;
