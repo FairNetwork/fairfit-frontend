3d61a6cb7395e221d45f8b44c9101804ef20803a
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-04-03 20:15:47 +0200

    ✨ Add homepage footer

--- src/components/home-view/HomeView.tsx
+++ src/components/home-view/HomeView.tsx
@@ -7,6 +7,16 @@ import { loadAllGyms } from '../../redux/gym/actions';
 import { selectGyms } from '../../redux/gym/selectors';
 import Search from './search/Search';
 import GymCard from './gym-card/GymCard';
+import Footer from '../shared/footer/Footer';
+import { FooterItem } from '../../types/footer';
+
+const FOOTER_ITEMS: FooterItem[] = [
+    { id: '1', name: 'Impressum', path: 'impressum' },
+    { id: '2', name: 'Datenschutz', path: 'datenschutz' },
+    { id: '3', name: 'Allgemein', path: 'allgemein' },
+    { id: '4', name: 'Studio anmelden', path: 'studio_anmelden' },
+    { id: '5', name: 'Q&A', path: 'q_&_a' }
+];
 
 const HomeView = () => {
     const dispatch = useAppDispatch();
@@ -53,6 +63,7 @@ const HomeView = () => {
                 <h5 className="home-view__content__headline">Deine Ergebnisse</h5>
                 {content}
             </div>
+            <Footer items={FOOTER_ITEMS} />
         </div>
     );
 };
