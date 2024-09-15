1f0c108efe51b33ff44a69413becc811a86be434
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-09-15 14:02:12 +0200

    ✨ Add booking info dialog

--- src/components/booking/booking-content/send-button/SendButton.tsx
+++ src/components/booking/booking-content/send-button/SendButton.tsx
@@ -1,21 +1,92 @@
 import './sendButton.scss';
-import { Button } from '@mui/material';
+import {
+    Button,
+    CircularProgress,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle
+} from '@mui/material';
 import { useAppDispatch, useAppSelector } from '../../../../hooks/redux';
 import {
     selectIsButtonDisabled,
     selectSendOrderLoadingState
 } from '../../../../redux/user/selectors';
 import { sendSubscription } from '../../../../redux/user/actions';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { setSendOrderLoadingState } from '../../../../redux/user/slice';
+import { selectCurrentGymId } from '../../../../redux/gym/selectors';
+import { useNavigate } from 'react-router-dom';
+import DialogTransition from '../../../shared/dialog-transition/DialogTransition';
 
 const SendButton = () => {
     const dispatch = useAppDispatch();
 
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
+
     const isButtonDisabled = useAppSelector(selectIsButtonDisabled);
     const sendOrderLoadingState = useAppSelector(selectSendOrderLoadingState);
+    const currentGymId = useAppSelector(selectCurrentGymId);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (sendOrderLoadingState !== 'none') {
+            setIsDialogOpen(true);
+        }
+    }, [sendOrderLoadingState]);
 
     const handleClick = () => {
         void dispatch(sendSubscription());
     };
+
+    const handleCloseDialog = useCallback(() => {
+        switch (sendOrderLoadingState) {
+            case 'successful':
+                navigate(`/${currentGymId}`);
+                dispatch(setSendOrderLoadingState('none'));
+
+                break;
+            case 'pending':
+                setIsDialogOpen(false);
+                break;
+            case 'rejected':
+                setIsDialogOpen(false);
+                dispatch(setSendOrderLoadingState('none'));
+
+                break;
+            default:
+                break;
+        }
+    }, [currentGymId, dispatch, navigate, sendOrderLoadingState]);
+
+    const dialogTitle = useMemo(() => {
+        switch (sendOrderLoadingState) {
+            case 'successful':
+                return <DialogTitle>Deine Bestellung wurde erfolgreich abgegeben</DialogTitle>;
+            case 'rejected':
+                return <DialogTitle>Deine Bestellung konnte nicht abgegeben werden</DialogTitle>;
+            default:
+                return null;
+        }
+    }, [sendOrderLoadingState]);
+
+    const dialogContent = useMemo(() => {
+        switch (sendOrderLoadingState) {
+            case 'pending':
+                return (
+                    <DialogContent>
+                        <CircularProgress />
+                    </DialogContent>
+                );
+            default:
+                return (
+                    <DialogActions>
+                        <Button onClick={handleCloseDialog}>OK</Button>
+                    </DialogActions>
+                );
+        }
+    }, [handleCloseDialog, sendOrderLoadingState]);
 
     return (
         <div className="send-button">
@@ -25,6 +96,15 @@ const SendButton = () => {
                 onClick={handleClick}>
                 Abschließen
             </Button>
+            <Dialog
+                open={isDialogOpen}
+                keepMounted
+                TransitionComponent={DialogTransition}
+                onClose={handleCloseDialog}
+                aria-describedby="alert-dialog-slide-description">
+                {dialogTitle}
+                {dialogContent}
+            </Dialog>
         </div>
     );
 };
--- src/redux/user/actions.ts
+++ src/redux/user/actions.ts
@@ -1,6 +1,6 @@
 import { AppDispatch, GetAppState } from '../store';
 import { selectSelectedOfferName, selectUser } from './selectors';
-import { setUser } from './slice';
+import { setSendOrderLoadingState, setUser } from './slice';
 import { postSubscription } from '../../api/subscriptions/post';
 import { selectCurrentGymId } from '../gym/selectors';
 
@@ -8,6 +8,8 @@ export const sendSubscription =
     () =>
     async (dispatch: AppDispatch, getState: GetAppState): Promise<void> => {
         const state = getState();
+
+        dispatch(setSendOrderLoadingState('pending'));
 
         const user = selectUser(state);
         const abonnementName = selectSelectedOfferName(state);
@@ -21,5 +23,10 @@ export const sendSubscription =
 
         if (status === 201) {
             dispatch(setUser({}));
+            dispatch(setSendOrderLoadingState('successful'));
+
+            return;
         }
+
+        dispatch(setSendOrderLoadingState('rejected'));
     };
