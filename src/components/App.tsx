08668dc0bb9f55aa641a7c82c88046fc1e336f1b
Michael Gesenhues <michael.gesenhues@tobit.com>
2025-01-18 14:44:31 +0100

    ✨ Add colormode hook

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,17 @@
 import { useContent } from '../hooks/content';
 import ColorSchemeProvider from './shared/color-scheme-provider/ColorSchemeProvider';
 import { THEME } from '../constants/theme';
+import { useColorMode } from '../hooks/color';
 
 const App = () => {
     const content = useContent();
+    const colorMode = useColorMode();
 
-    return <ColorSchemeProvider colors={THEME}>Test</ColorSchemeProvider>;
+    return (
+        <ColorSchemeProvider colors={THEME} colorMode={colorMode}>
+            Test
+        </ColorSchemeProvider>
+    );
 };
 
 App.displayNAme = 'App';
