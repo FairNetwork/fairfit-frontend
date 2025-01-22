32e8796da810b5bbabb59e8ea8182a07d5b44d49
Michael Gesenhues <michael.gesenhues@tobit.com>
2025-01-22 08:57:57 +0100

    ✨ Add splashscreen

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,13 +6,27 @@ import Sidebar from './shared/sidebar/Sidebar';
 import Header from './shared/header/Header';
 import Footer from './shared/footer/Footer';
 import './app.scss';
+import { useLayoutEffect, useState } from 'react';
+import { AnimatePresence } from 'framer-motion';
+import SplashScreen from './shared/splash-screen/SplashScreen';
 
 const App = () => {
     const content = useContent();
     const colorMode = useColorMode();
 
+    const [shouldShowSplashScreen, setShouldShowSplashScreen] = useState(true);
+
+    useLayoutEffect(() => {
+        window.setTimeout(() => {
+            setShouldShowSplashScreen(false);
+        }, 3000);
+    }, []);
+
     return (
         <ColorSchemeProvider colors={THEME} colorMode={colorMode}>
+            <AnimatePresence initial={false}>
+                {shouldShowSplashScreen && <SplashScreen />}
+            </AnimatePresence>
             <div className="app">
                 <Sidebar />
                 <div className="app__wrapper">
