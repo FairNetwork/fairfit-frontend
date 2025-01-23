d1fe295687a4b93dc504cce62fe8a8f112c33496
Michael Gesenhues <michael.gesenhues@tobit.com>
2025-01-23 14:37:17 +0100

    💄 Update styles

--- src/components/shared/sidebar/user/User.tsx
+++ src/components/shared/sidebar/user/User.tsx
@@ -5,10 +5,20 @@ import { useNavigate } from 'react-router-dom';
 const User = () => {
     const navigate = useNavigate();
 
+    const isLoggedIn = true;
+
+    const handleClick = () => {
+        if (isLoggedIn) {
+            // ToDo add logout
+        } else {
+            navigate('/log-in');
+        }
+    };
+
     return (
         <div className="user">
             <div className="user__login-button">
-                <Button onClick={() => navigate('/log-in')}>Anmelden</Button>
+                <Button onClick={handleClick}>{isLoggedIn ? 'Abmelden' : 'Anmelden'}</Button>
             </div>
         </div>
     );
