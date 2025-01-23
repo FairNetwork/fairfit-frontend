edcff0660e489aa57ca8a604ef49c4107e2ad0a5
Michael Gesenhues <michael.gesenhues@tobit.com>
2025-01-23 21:19:41 +0100

    ✨ Add socialmedia settings

--- src/view/dashboard/dashboard-settings/DashboardSettings.tsx
+++ src/view/dashboard/dashboard-settings/DashboardSettings.tsx
@@ -1,10 +1,12 @@
 import './dashboardSettings.scss';
 import GeneralSettings from './general-settings/GeneralSettings';
+import SocialMediaSettings from './social-media-settings/SocialMediaSettings';
 
 const DashboardSettings = () => {
     return (
         <div className="dashboard-settings">
             <GeneralSettings />
+            <SocialMediaSettings />
         </div>
     );
 };
