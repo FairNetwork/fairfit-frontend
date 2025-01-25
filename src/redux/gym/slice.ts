86efc31d79e16421401d2350d1e7ccdc328661bb
Michael Gesenhues <michael.gesenhues@tobit.com>
2025-01-25 13:17:08 +0100

    ✨ Add mock data

--- src/redux/gym/slice.ts
+++ src/redux/gym/slice.ts
@@ -18,12 +18,40 @@ const initialState: GymState = {
             address: 'Ahaus',
             image: 'https://images.stockcake.com/public/a/4/f/a4fb5f0b-ff65-4b44-8de8-e9d53c5c0a7f_large/intense-gym-workout-stockcake.jpg'
         },
+        bodyfit: {
+            name: 'BodyFit',
+            id: 'bodyfit',
+            type: GymType.GYM,
+            address: 'Ahaus',
+            image: 'https://images.stockcake.com/public/a/4/f/a4fb5f0b-ff65-4b44-8de8-e9d53c5c0a7f_large/intense-gym-workout-stockcake.jpg'
+        },
+        shield: {
+            name: 'Shield',
+            id: 'shield',
+            type: GymType.GYM,
+            address: 'Ahaus',
+            image: 'https://images.stockcake.com/public/a/4/f/a4fb5f0b-ff65-4b44-8de8-e9d53c5c0a7f_large/intense-gym-workout-stockcake.jpg'
+        },
+        'clever-fit': {
+            name: 'Clever Fit',
+            id: 'clever-fit',
+            type: GymType.GYM,
+            address: 'Ahaus',
+            image: 'https://images.stockcake.com/public/a/4/f/a4fb5f0b-ff65-4b44-8de8-e9d53c5c0a7f_large/intense-gym-workout-stockcake.jpg'
+        },
         'eintracht-ahaus': {
             name: 'Eintracht Ahaus',
             id: 'eintracht-ahaus',
             type: GymType.FOOTBALL,
             address: 'Ahaus',
             image: 'https://play-lh.googleusercontent.com/MdQuO3eGMwkERzTrxlbNfbDQdvQUL6Rey-fLFzwpFqMnCnWdeJxmP9RGxIxWGbf9CH0=w648-h364-rw'
+        },
+        aquahaus: {
+            name: 'AquAHAUS',
+            id: 'aquahaus',
+            type: GymType.SWIMMING,
+            address: 'Ahaus',
+            image: 'https://www.carpediem.life/files/article/schwimmen.jpg'
         }
     }
 };
